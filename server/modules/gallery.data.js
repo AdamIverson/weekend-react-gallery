989de3f53bfd1e6beb6af7e93aa645da67aa94df
Adam Iverson <aciverso@gmail.com>
2021-11-27 16:37:21 -0600

    conditional rendering is functioning

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -8,37 +8,43 @@ const galleryItems = [
         id: 1, 
         path: '../images/june_10_2021_film_b-1.jpg', 
         description: 'one half of the front wheel of a motorcycle and the shadow of the motorcyclist passing under a bridge', 
-        likes: 0 
+        likes: 0,
+        display: true
     },
     { 
         id: 2, 
         path: '../images/june_10_2021_film_blossoms-1.jpg', 
         description: 'pink spring blossoms', 
-        likes: 0 
+        likes: 0,
+        display: true
     },
     { 
         id: 3, 
         path: '../images/june_10_2021_film_orange_cat-2.jpg', 
         description: 'an orange cat sits outside with green vegetation', 
-        likes: 0 
+        likes: 0,
+        display: true
     },
     { 
         id: 4, 
         path: '../images/june_10_2021_tandem-1.jpg', 
         description: 'the most south minneapolis thing ive ever seen', 
-        likes: 0 
+        likes: 0,
+        display: true
     },
     { 
         id: 5, 
         path: '../images/march_07_2021_-5.jpg', 
         description: 'glove on a stick in the blue sky', 
-        likes: 0 
+        likes: 0,
+        display: true 
     },
     { 
         id: 6, 
         path: '../images/R1-01224-0015.jpg', 
         description: 'an old friend drinks a drink in a steakhouse in old saint paul', 
-        likes: 0 
+        likes: 0,
+        display: true
     }
 ];
 
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,40 +3,54 @@ import GalleryList from '../GalleryList/GalleryList'
 import axios from 'axios'
 import './GalleryItem.css'
 
-// const refreshLikes = ({fetchGallery}) => {
-//     fetchGallery();
-// }
 function GalleryItem({ photo, fetchGallery }) {
     console.log('in GalleryItem');
+const [display, setDisplay] = useState(true);
 
-    // const refreshLikes = ({fetchGallery}) => {
-    //     fetchGallery();
-    // }
+const addLike = () => {
 
-    const addLike = () => {
+    axios({
+        method: 'PUT',
+        url: `/gallery/like/${photo.id}`
+    }).then((response) => {
+        console.log(photo.likes);
+        fetchGallery();
+    }).catch((error) => {
+        console.log('PUT route failed');
+    }); 
+};
 
-        axios({
-            method: 'PUT',
-            url: `/gallery/like/${photo.id}`
-        }).then((response) => {
-            console.log(photo.likes);
-            fetchGallery();
-        }).catch((error) => {
-            console.log('PUT route failed');
-        }); 
-    };
+const flipDisplay = () => {
+    if (display) {
+        setDisplay(false);
+    } else {
+        setDisplay(true);
+    }
+    }
 
-
-    return (
-        <div>
-            <img  src={photo.path}/>
+    if(display) {
+        return (
+            <div>
+            <p onClick={flipDisplay}>{photo.description}</p>
             <button 
                 onClick={addLike}
                 key={photo.id}
                 >love it!</button>
                 <p>{photo.likes} people love this</p>
         </div>
-    )
+        )
+    } else {
+    return (
+        <div>
+            <img onClick={flipDisplay} src={photo.path}/>
+            <button 
+                onClick={addLike}
+                key={photo.id}
+                >love it!</button>
+                <p>{photo.likes} people love this</p>
+        </div>
+    )};
 }
+
 
 export default GalleryItem;
